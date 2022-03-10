56a544dafd4f0bad491340ec8ba7a0aa670e3da7
Aryan <93074700+useraid@users.noreply.github.com>
2022-03-10 09:43:25 +0530

    added programs

--- DebloatUpdate.sh
+++ DebloatUpdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Debloat and Update Script for Ubuntu 20.04 LTS"
 echo "Debloating First......"
-sudo apt remove --purge gnome-mahjongg gnome-mines gnome-sudoku aisleriot thunderbird* rhythmbox* deja-dup libreoffice* totem cheese transmission* usb-creator* shotwell* remmina* gnome-todo gnome-shell-extension-ubuntu-dock -y
+sudo apt remove --purge gnome-mahjongg gnome-mines gnome-sudoku aisleriot thunderbird* rhythmbox* deja-dup libreoffice* totem cheese transmission* usb-creator* shotwell* remmina* gnome-todo gnome-shell-extension-ubuntu-dock pidgin hexchat simple-scan eog gimp -y
 sudo apt autoremove -y
 sudo apt autoclean -y
 echo "Updating Now......"
