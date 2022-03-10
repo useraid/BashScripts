e4000787bf805899160d68e206a6f69e9f556bbf
Aryan <93074700+useraid@users.noreply.github.com>
2022-03-10 14:45:25 +0530

    added outputs

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+echo "Setting time Server"
 timedatectl set-ntp true
 cfdisk /dev/sda
 mkfs.ext4 /dev/sda1
+echo "Creating Swap"
 mkswap /dev/sda2
+echo "Mounting Drives"
 mount /dev/sda1 /mnt
 swapon /dev/sda2
+echo "Updating Arch Keyring"
 pacman -S archlinux-keyring -y
+echo "Getting Arch Files"
 pacstrap /mnt base linux linux-firmware -y
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
@@ -22,6 +27,7 @@ echo "
 127.0.1.1	archvm " > /etc/hosts
 pacman -S archlinux-keyring
 pacman -S grub os-prober
+echo "Installing Grub"
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 echo "Create Password (Note : password is not visible for security reasons)"
